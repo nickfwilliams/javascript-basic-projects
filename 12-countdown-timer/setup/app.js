c33f6d3893a2d9f176b606e388897d0d9f28f5b5
Nick Williams <nick.williams@bristol.ac.uk>
2024-11-15 16:08:59 +0000

    Complete giveaway end date

--- 12-countdown-timer/setup/app.js
+++ 12-countdown-timer/setup/app.js
@@ -21,3 +21,23 @@ const weekdays = [
 	"Friday",
 	"Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2025, 5, 28, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+
+let month = futureDate.getMonth();
+month = months[month];
+
+const weekday = weekdays[futureDate.getDay()];
+
+const date = futureDate.getDate();
+
+giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
